bc26c2656e1128da64424c5e1fbd0f99878e2d61
ANGIELOUX <angelajohnson943@gmail.com>
2022-07-31 22:37:38 +1000

    Add header/footer to all pages

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
-import React, {useReducer} from 'react'
+import React, {useReducer, useEffect} from 'react'
 import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 import Products from './components/Products';
-// import { GlobalStyle } from './styled-components/globalStyles';
 import Product from './components/Product';
 import { LoginForm } from './components/LoginForm';
-// import ResponsiveAppBar from './components/nav/Nav';
 import globalReducer from './context/globalReducer';
 import initialState from './context/initialState';
 import { GlobalContext } from './context/globalContext';
-// import { getProducts } from './services/productServices';
-// import { retrieveUserFromJWT } from './services/userServices';
+import { retrieveUserFromJWT } from './services/userServices';
 import './App.scss'
 import {Header} from './components/header/Header'
 import {Hero} from './components/hero/Hero'
-
-
+import NotFound from './components/NotFound';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   const [globalStore, globalDispatch] = useReducer(globalReducer, initialState);
   const {user} = globalStore;
-  // const token = sessionStorage.getItem('jwt');
-  console.log(globalStore)
-  // useEffect(() => {
-  //   getProducts()
-  //   .then(products => {
-  //     dispatch({type: `setProducts`, data: products})})
-  //   .catch(error => console.error(error))
-  //   // eslint-disable-next-line
-  // },[]) 
+  const token = sessionStorage.getItem('jwt');
 
-  // useEffect(() => {
-  //   retrieveUserFromJWT()
-  //   .then(response => dispatch({type: 'setLoggedInUser', data: response.username}))
-  // }, [token])
+  useEffect(() => {
+    retrieveUserFromJWT()
+    .then(response => globalDispatch({type: 'setLoggedInUser', data: response.username}))
+  }, [token])
 
   return (
     <>
-      {/* <GlobalStyle/> */}
 
       <GlobalContext.Provider value={{globalStore, globalDispatch}}>
       <BrowserRouter>
@@ -45,6 +33,7 @@ const App = () => {
       <Hero><p>{user.username}</p></Hero>
       <Routes>
       <Route path="/" element={<Navigate to="/products"/>}/>
+      <Route path="*" element={<NotFound/>}/>
       <Route path="/products" element={<Products/>}/>
       <Route path="/products/:id" element={<Product />}/>
       <Route path="/auth/login" element={<LoginForm />}/>
@@ -52,7 +41,8 @@ const App = () => {
 
       </Routes>
       
-      </BrowserRouter>    
+      </BrowserRouter>   
+      <Footer/>
       </GlobalContext.Provider>
     </>
   )
